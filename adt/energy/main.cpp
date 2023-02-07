a37d23ad283ec538ad4d2102d7e80a66cdca1dec
RinaBoni <brisovae@mail.ru>
2023-02-07 13:14:33 +0900

    с компа

---
+++ adt/energy/main.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <string>
+#include "energy.h"
+#include <stdio.h>
+
+using namespace std;
+
+
+int main()
+{
+    try {
+        EnergyDrink first("Monster", "Ultra enerdy", "cherry", 54, 32, 230, 100, 0.4);
+        //EnergyDrink first;
+        //EnergyDrink. 
+        //first = EnergyDrink();
+        cout<< first.AllInformation();
+    }
+    catch (exception& argument) {
+        cout << argument.what();
+    }
+
+    //EnergyDrink();
+    EnergyDrink monster;
+   
+    //count << monster.AllInformation();
+
+
+
+    return 0;
+}
